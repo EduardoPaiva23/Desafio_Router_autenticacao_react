562d55ab30f210227cfa4a280fd891dee1e381b5
eduardopaiva420 <eduardok86@gmail.com>
2021-07-17 18:26:01 -0300

    Primeiro commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+import { ToggleProvider } from './context/ToggleProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToggleProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ToggleProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
